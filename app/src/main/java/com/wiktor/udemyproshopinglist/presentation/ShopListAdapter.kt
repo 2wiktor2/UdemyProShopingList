a44f9cae8a6579a6a4e6a0df557b0eb22369e1c2
wiktor <wiktor@bk.ru>
2022-08-30 13:10:29 +0300

    lesson_25 добавлен адаптер pause

--- app/src/main/java/com/wiktor/udemyproshopinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/wiktor/udemyproshopinglist/presentation/ShopListAdapter.kt
@@ -8,12 +8,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.wiktor.udemyproshopinglist.R
 import com.wiktor.udemyproshopinglist.domain.ShopItem
 
-class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
     val list = listOf<ShopItem>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_desabled, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_shop_desabled, parent, false)
         return ShopItemViewHolder(view)
     }
 
@@ -30,7 +31,12 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
         return list.size
     }
 
-    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+    }
+
+    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName = view.findViewById<TextView>(R.id.textView_name)
         val tvCount = view.findViewById<TextView>(R.id.textView_count)
     }
